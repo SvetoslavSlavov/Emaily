57b2b16678ba51a13f4b9819b30904c1df59bd6c
Svetoslav Slavov <svetoslav_slavov@icloud.com>
2017-12-28 00:32:32 +0200

    Heroku Deployment getting ready.

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // Route handler with express
-app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
+app.get("/", (req, res) => {
+  res.send({ hi: "there" });
 });
-
-app.listen(5000);
+/**
+ * Use it in production enviroment or in development
+ */
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
