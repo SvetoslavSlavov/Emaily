edb1f8c388828a6f357f5908c13f60ba498674a0
Svetoslav Slavov <svetoslav_slavov@icloud.com>
2017-12-30 18:42:42 +0200

    PasspordJS Set up

--- server/index.js
+++ server/index.js
@@ -1,12 +1,20 @@
-const express = require("express");
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
+passport.use(new GoogleStrategy());
+
 // Route handler with express
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+// app.get("/", (req, res) => {
+//   res.send({ hi: "there" });
+// });
 /**
  * Use it in production enviroment or in development
  */
+
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
