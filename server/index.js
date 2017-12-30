187970c4b4bd81534f8f0fdb4a0350f53d92efea
Svetoslav Slavov <svetoslav_slavov@icloud.com>
2017-12-30 19:03:02 +0200

    Enabling Google OAuth API

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
-const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 const app = express();
 
 passport.use(new GoogleStrategy());
+
+// clientID 328377180571-tgqhuhqfnfmiri6elnb7gma738t5f3dp.apps.googleusercontent.com
+// clientSecret 0OPFQr4SsSyJsQJqSdeApD4y
 
 // Route handler with express
 // app.get("/", (req, res) => {
@@ -13,8 +16,6 @@ passport.use(new GoogleStrategy());
 /**
  * Use it in production enviroment or in development
  */
-
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
