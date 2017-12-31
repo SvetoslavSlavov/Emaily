d5aea9fadd730741316768a7ae03b7a8bc74de23
Svetoslav Slavov <svetoslav_slavov@icloud.com>
2017-12-31 02:15:00 +0200

    Google Strategy Options

--- server/index.js
+++ server/index.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
 
 const app = express();
 
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      // user comming back from google
+      callbackURL: "/auth/google/callback"
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
 
 // clientID 328377180571-tgqhuhqfnfmiri6elnb7gma738t5f3dp.apps.googleusercontent.com
 // clientSecret 0OPFQr4SsSyJsQJqSdeApD4y
